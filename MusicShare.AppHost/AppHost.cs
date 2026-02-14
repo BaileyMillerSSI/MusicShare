3960bdd3c2be4ac67d38d3f0cf9dc33ca7860b55
Bailey Miller <baileymiller@live.com>
2026-02-14 17:21:22 -0500

    rename infra params

--- MusicShare.AppHost/AppHost.cs
+++ MusicShare.AppHost/AppHost.cs
@@ -89,7 +89,7 @@ IResourceBuilder<IResourceWithConnectionString> ConfigureMongoDb()
     }
     else
     {
-        return builder.AddConnectionString("mongodb", "AZURE_MONGODB_URI");
+        return builder.AddConnectionString("mongodb", "AZURE_MONGODB");
     }
 }
 
@@ -104,6 +104,6 @@ IResourceBuilder<IResourceWithConnectionString> ConfigureRabbitMQ()
     }
     else
     {
-        return builder.AddConnectionString("rabbitmq", "AZURE_RABBITMQ_URL");
+        return builder.AddConnectionString("rabbitmq", "AZURE_RABBITMQ");
     }
 }
